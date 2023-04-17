61900a911ca11162fffdec432818e3fe61a5afba
jasperls <jasperlutzseverino@gmail.com>
2023-04-17 20:16:27 +0200

    style: turn ro spotify service into record

--- src/main/java/cat/lasallegracia/spotify/ReadOnlySpotifyService.java
+++ src/main/java/cat/lasallegracia/spotify/ReadOnlySpotifyService.java
@@ -4,12 +4,7 @@ import lombok.Getter;
 import se.michaelthelin.spotify.SpotifyApi;
 
 @Getter
-public class ReadOnlySpotifyService {
-    private final SpotifyApi spotifyApi;
-
-    public ReadOnlySpotifyService(SpotifyApi spotifyApi) {
-        this.spotifyApi = spotifyApi;
-    }
+public record ReadOnlySpotifyService(SpotifyApi spotifyApi) {
 
     // useful abstraction for utility methods?
 }
