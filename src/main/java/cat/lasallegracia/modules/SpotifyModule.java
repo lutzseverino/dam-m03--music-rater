307ce69cfdbae1f15eb462f32bccb9586682d927
jasperls <jasperlutzseverino@gmail.com>
2023-04-17 18:50:26 +0200

    style: move spotify api build logic to module

--- src/main/java/cat/lasallegracia/modules/SpotifyModule.java
+++ src/main/java/cat/lasallegracia/modules/SpotifyModule.java
@@ -1,17 +1,36 @@
 package cat.lasallegracia.modules;
 
 import cat.lasallegracia.spotify.ReadOnlySpotifyService;
+import cat.lasallegracia.spotify.SpotifyClientConfig;
+import cat.lasallegracia.yaml.Snakelet;
 import com.google.inject.AbstractModule;
+import com.google.inject.Provides;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import se.michaelthelin.spotify.SpotifyApi;
 
 public class SpotifyModule extends AbstractModule {
-    private final ReadOnlySpotifyService readOnlySpotifyService;
+    private static final Logger logger = LoggerFactory.getLogger(SpotifyModule.class);
+    private final String configPath;
 
-    public SpotifyModule(ReadOnlySpotifyService readOnlySpotifyService) {
-        this.readOnlySpotifyService = readOnlySpotifyService;
+
+    public SpotifyModule(String configPath) {
+        this.configPath = configPath;
     }
 
-    @Override
-    protected void configure() {
-        bind(ReadOnlySpotifyService.class).toInstance(readOnlySpotifyService);
+    @Provides public ReadOnlySpotifyService provideReadOnlySpotifyService() {
+        SpotifyClientConfig spotifyClientConfig = new Snakelet(configPath).read(SpotifyClientConfig.class);
+
+        if (spotifyClientConfig == null) {
+            logger.error("Can't initialize: Can't read config file: \"" + configPath + "\", it's either missing or malformed.");
+            System.exit(1);
+        }
+
+        return new ReadOnlySpotifyService(
+                new SpotifyApi.Builder()
+                        .setClientId(spotifyClientConfig.getCLIENT_ID())
+                        .setClientSecret(spotifyClientConfig.getCLIENT_SECRET())
+                        .build()
+        );
     }
 }
--- src/main/java/cat/lasallegracia/spotify/ReadOnlySpotifyService.java
+++ src/main/java/cat/lasallegracia/spotify/ReadOnlySpotifyService.java
@@ -5,14 +5,11 @@ import se.michaelthelin.spotify.SpotifyApi;
 
 @Getter
 public class ReadOnlySpotifyService {
-    private final SpotifyClientConfig spotifyClientConfig;
     private final SpotifyApi spotifyApi;
 
-    public ReadOnlySpotifyService(SpotifyClientConfig spotifyClientConfig) {
-        this.spotifyClientConfig = spotifyClientConfig;
-        this.spotifyApi = new SpotifyApi.Builder()
-                .setClientId(spotifyClientConfig.getCLIENT_ID())
-                .setClientSecret(spotifyClientConfig.getCLIENT_SECRET())
-                .build();
+    public ReadOnlySpotifyService(SpotifyApi spotifyApi) {
+        this.spotifyApi = spotifyApi;
     }
+
+    // useful abstraction for utility methods?
 }
