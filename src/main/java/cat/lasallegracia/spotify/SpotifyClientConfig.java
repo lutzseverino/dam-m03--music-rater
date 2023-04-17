f0f4fde7ed7074feda65870bf0aa7e28512d98ca
jasperls <jasperlutzseverino@gmail.com>
2023-04-17 18:51:01 +0200

    docs: explain json property annotation

--- src/main/java/cat/lasallegracia/spotify/SpotifyClientConfig.java
+++ src/main/java/cat/lasallegracia/spotify/SpotifyClientConfig.java
@@ -4,11 +4,15 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import lombok.Setter;
 
+/**
+ * This class is used to read the Spotify client configuration from the config.yaml file.
+ * <p>
+ * The @JsonProperty annotation is required to map the config file
+ * keys to the class fields with proper casing.
+ */
 @Getter
 @Setter
 public class SpotifyClientConfig {
-    @JsonProperty("CLIENT_ID")
-    private String CLIENT_ID;
-    @JsonProperty("CLIENT_SECRET")
-    private String CLIENT_SECRET;
+    @JsonProperty("CLIENT_ID") private String CLIENT_ID;
+    @JsonProperty("CLIENT_SECRET") private String CLIENT_SECRET;
 }
