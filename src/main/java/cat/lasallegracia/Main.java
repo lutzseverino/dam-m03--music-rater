424ce6971ae7370fdd0559108d5264a334b48b7a
jasperls <jasperlutzseverino@gmail.com>
2023-04-17 20:16:06 +0200

    refactor: main class

--- src/main/java/cat/lasallegracia/Main.java
+++ src/main/java/cat/lasallegracia/Main.java
@@ -2,15 +2,16 @@ package cat.lasallegracia;
 
 import cat.lasallegracia.modules.SpotifyModule;
 import com.google.inject.Guice;
+import com.google.inject.Injector;
 
 public enum Main {
     INSTANCE;
 
     public static void main(String[] args) {
         String configPath = args.length > 0 ? args[0] : "src/main/resources/config.yaml";
-        var injector = Guice.createInjector(new SpotifyModule(configPath));
-
+        Injector injector = Guice.createInjector(new SpotifyModule(configPath));
         JavaSimpleMusicRater instance = injector.getInstance(JavaSimpleMusicRater.class);
+
         instance.start();
     }
 
