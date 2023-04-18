337eff6c419d19786fb552fa76a03a1bb1c506c9
jasperls <jasperlutzseverino@gmail.com>
2023-04-18 21:28:37 +0200

    fix: init spotify api with client creds

--- src/main/java/cat/lasallegracia/modules/SpotifyModule.java
+++ src/main/java/cat/lasallegracia/modules/SpotifyModule.java
@@ -5,9 +5,13 @@ import cat.lasallegracia.spotify.SpotifyClientConfig;
 import cat.lasallegracia.yaml.Snakelet;
 import com.google.inject.AbstractModule;
 import com.google.inject.Provides;
+import org.apache.hc.core5.http.ParseException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import se.michaelthelin.spotify.SpotifyApi;
+import se.michaelthelin.spotify.exceptions.SpotifyWebApiException;
+
+import java.io.IOException;
 
 public class SpotifyModule extends AbstractModule {
     private static final Logger logger = LoggerFactory.getLogger(SpotifyModule.class);
@@ -22,15 +26,21 @@ public class SpotifyModule extends AbstractModule {
         SpotifyClientConfig spotifyClientConfig = new Snakelet(configPath).read(SpotifyClientConfig.class);
 
         if (spotifyClientConfig == null) {
-            logger.error("Can't initialize: Can't read config file: \"" + configPath + "\", it's either missing or malformed.");
+            logger.error("Module can't provide: Config file \"" + configPath + "\" is either missing or malformed. Program will exit.");
             System.exit(1);
         }
 
-        return new ReadOnlySpotifyService(
-                new SpotifyApi.Builder()
-                        .setClientId(spotifyClientConfig.getCLIENT_ID())
-                        .setClientSecret(spotifyClientConfig.getCLIENT_SECRET())
-                        .build()
-        );
+        SpotifyApi spotifyApi = new SpotifyApi.Builder()
+                .setClientId(spotifyClientConfig.getCLIENT_ID())
+                .setClientSecret(spotifyClientConfig.getCLIENT_SECRET())
+                .build();
+
+        try {
+            spotifyApi.setAccessToken(spotifyApi.clientCredentials().build().execute().getAccessToken());
+        } catch (IOException | SpotifyWebApiException | ParseException e) {
+            throw new RuntimeException(e);
+        }
+
+        return new ReadOnlySpotifyService(spotifyApi);
     }
 }
