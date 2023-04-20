5238ead3038c8dfb96244a0381fa92f06b35190e
jasperls <jasperlutzseverino@gmail.com>
2023-04-20 13:52:53 +0200

    fix: center panel components

--- src/main/java/cat/lasallegracia/windows/HomeSelection.java
+++ src/main/java/cat/lasallegracia/windows/HomeSelection.java
@@ -1,6 +1,7 @@
 package cat.lasallegracia.windows;
 
 import cat.lasallegracia.i18n.ResourceBundleManager;
+import com.googlecode.lanterna.TerminalSize;
 import com.googlecode.lanterna.gui2.*;
 import com.googlecode.lanterna.gui2.dialogs.ActionListDialogBuilder;
 
@@ -8,14 +9,21 @@ import javax.inject.Inject;
 import java.util.List;
 
 public class HomeSelection extends BasicWindow {
+
     @Inject public HomeSelection(ResourceBundleManager language) {
+        final LayoutData CENTERED_LAYOUT = LinearLayout.createLayoutData(LinearLayout.Alignment.Center);
+
         setHints(List.of(Hint.CENTERED));
 
         Panel panel = new Panel();
         panel.setLayoutManager(new LinearLayout(Direction.VERTICAL).setSpacing(2));
-        panel.setLayoutData(LinearLayout.createLayoutData(LinearLayout.Alignment.Center));
 
-        Label title = new Label(language.getString("home.title"));
+        Label title = new Label(language.getString("info.title"));
+        title.setLayoutData(CENTERED_LAYOUT);
+
+        Label about = new Label(language.getString("info.about"));
+        about.setLayoutData(CENTERED_LAYOUT);
+
         Component dialog = new ActionListDialogBuilder()
                 .setDescription(language.getString("home.dialog.description"))
                 .addAction("Option 1", () -> {
@@ -24,8 +32,10 @@ public class HomeSelection extends BasicWindow {
                 .addAction("Option 2", () -> {
                     // Do something for Option 2
                 }).build().getComponent();
+        dialog.setLayoutData(CENTERED_LAYOUT);
 
         panel.addComponent(title);
+        panel.addComponent(about);
         panel.addComponent(dialog);
 
         setComponent(panel);
