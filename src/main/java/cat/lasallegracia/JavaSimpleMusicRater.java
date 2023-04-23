2303f109a5a24b92e9b478727d83a3cde29de522
Jasper Lutz Severino <jasperlutzseverino@gmail.com>
2023-04-23 19:31:59 +0200

    build: revert lanterna and jline

--- src/main/java/cat/lasallegracia/JavaSimpleMusicRater.java
+++ src/main/java/cat/lasallegracia/JavaSimpleMusicRater.java
@@ -1,32 +1,8 @@
 package cat.lasallegracia;
 
-import cat.lasallegracia.windows.HomeSelection;
-import com.google.inject.Inject;
-import com.googlecode.lanterna.gui2.MultiWindowTextGUI;
-import com.googlecode.lanterna.gui2.WindowBasedTextGUI;
-import com.googlecode.lanterna.screen.Screen;
-import com.googlecode.lanterna.screen.TerminalScreen;
-import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
-import com.googlecode.lanterna.terminal.Terminal;
-
-import java.io.IOException;
-
 public class JavaSimpleMusicRater {
 
-    @Inject private HomeSelection homeSelection;
-
     public void start() {
-        try {
-            // TODO: Implement, this is just a test!
-            Terminal terminal = new DefaultTerminalFactory().createTerminal();
-            Screen screen = new TerminalScreen(terminal);
-            WindowBasedTextGUI textGUI = new MultiWindowTextGUI(screen);
 
-            screen.startScreen();
-            textGUI.addWindow(homeSelection);
-            homeSelection.waitUntilClosed();
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
     }
 }
